b20a988b7f34e0dd0711db8b75728768f03ccb85
Choharim <jhl8586@naver.com>
2020-12-02 19:43:44 +0900

    Add class component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({fav, picture}){
-  return (
-    <div>
-      <h3>I love {fav}</h3>
-      <img src={picture}></img>
-    </div>
-  );
-}
 
-const foodILike = [
-  {
-    name:"kimchi",
-    image:"https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg"
-  },
-  {
-    name:"samgyupsal",
-    image:"https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg"
+class App extends React.Component{
+  state = {
+    count:0
+  };
+
+  render(){
+    return <h1>I am a class Component</h1>; 
   }
-];
-
-function renderFood(food){
-  return (
-    <Food fav={food.name} picture={food.image}/>
-  );
-}
-function App() {
-  return (
-    <div>
-      {foodILike.map(renderFood)}
-    </div>
-  );
 }
 
 export default App;
