567ee1ec0057ef8d3e126cc88b28b7463afa0014
Choharim <jhl8586@naver.com>
2020-12-04 17:31:24 +0900

    Install gh-pages, homepage, predeploy, build folder, deploy

--- src/Movie.js
+++ src/Movie.js
@@ -9,8 +9,8 @@ function Movie({id, year, title, summary, poster,genres}){
     <div className="movie_data">
       <h3 className="movie_title">{title}</h3>
       <h5 className="movie_year">{year}</h5>
-      <ul className="movie_genres">{genres.map((genre,index) => <li className="genres_genre" key={index}>{genre}</li>)}</ul> 
-      <p className="movie_summary">{summary}</p>
+      <ul className="movie_genres">{genres.map((genre,index) => <li className="genres_genre" key={index}>{genre}</li>)}</ul>
+      <p className="movie_summary">{summary.slice(0,150)}</p>
     </div>
   </div>
   );
