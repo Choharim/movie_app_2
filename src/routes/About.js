8959f2747e004525ccf0cb54c8f15cb27ba7c1b2
Choharim <jhl8586@naver.com>
2020-12-04 18:38:01 +0900

    Add Navigation component -Link to "

---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function About(){
+  return (
+    <span> This site show movie information. You can watch a lot of movies for free in this site. welcome to the movie site.</span>
+  );
+}
+
+export default About;
