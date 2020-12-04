8ef0c7c9c52171ab694b6320c7f688e0397d082c
Choharim <jhl8586@naver.com>
2020-12-04 17:57:46 +0900

    Add react-router-dom, HashRouter, Route

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import {HashRouter,Route}} from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
+
+function App(){
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route paht="/about" component={About}></Route>
+    </HashRouter>
+  );
+}
+
+export default App;
